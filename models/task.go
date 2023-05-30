e76a7f4726d608ecf7cfde27a0b816e199253d89
AliShan302 <ali.shan@wanclouds.net>
2023-05-30 12:31:23 +0500

    first commit

---
+++ models/task.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"time"
+
+	"github.com/fatih/structs"
+)
+
+type Task struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	CreatedAt   time.Time `json:"created_at"`
+	Status      string    `json:"status"`
+	Description string    `json:"description"`
+	Deadline    time.Time `json:"deadline"`
+	Priority    int       `json:"priority"`
+	Assignee    string    `json:"assignee"`
+}
+
+func (t *Task) Map() map[string]interface{} {
+	return structs.Map(t)
+}
+
+func (t *Task) Names() []string {
+	fields := structs.Fields(t)
+	names := make([]string, len(fields))
+	for i, field := range fields {
+		name := field.Name()
+		tagName := field.Tag(structs.DefaultTagName)
+		if tagName != "" {
+			name = tagName
+		}
+		names[i] = name
+	}
+	return names
+}
